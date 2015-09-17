ad6ed48149cf61ee577fb84d02501990ef082e34
Patrick Clary <pclary@outlook.com>
2015-09-17 16:26:11 -0700

    EKF uses magnetometer data

--- computejacobian.m
+++ computejacobian.m
@@ -1,7 +1,7 @@
 %% 
 syms q1 q2 q3 q4 g h real;
 
-h = quatrotate([0; 0; g], [q1; q2; q3; q4]);
+h = [quatrotate([0; 0; g], [q1; q2; q3; q4]); quatrotate([h; 0; 0], [q1; q2; q3; q4])];
 x = [q1; q2; q3; q4];
 
 H = jacobian(h, x)
