122b6722b22435337c80d33176d332541a0b1d3a
Patrick Clary <pclary@outlook.com>
2015-09-18 14:56:38 -0700

    Added accel and mag bias to filter

--- computejacobian.m
+++ computejacobian.m
@@ -1,8 +1,10 @@
 %% 
-syms q1 q2 q3 q4 wx wy wz g h real;
+syms q1 q2 q3 q4 wx wy wz ax ay az mx my mz g h real;
 
-h = [quatrotate([0; 0; g], [q1; q2; q3; q4]); quatrotate([h; 0; 0], [q1; q2; q3; q4]); wx; wy; wz];
-x = [q1; q2; q3; q4; wx; wy; wz];
+h = [quatrotate([0; 0; g], [q1; q2; q3; q4]) + [ax; ay; az]; 
+    quatrotate([h; 0; 0], [q1; q2; q3; q4]) + [mx; my; mz]; 
+    wx; wy; wz];
+x = [q1; q2; q3; q4; wx; wy; wz; ax; ay; az; mx; my; mz];
 
 H = jacobian(h, x)
 
