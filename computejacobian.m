b84d30a00283c2d09591d334c789ddb5ad5f8d5f
Patrick Clary <pclary@outlook.com>
2015-09-18 10:22:18 -0700

    Gyro is treated as a measurement instead of part of the model

--- computejacobian.m
+++ computejacobian.m
@@ -1,8 +1,8 @@
 %% 
-syms q1 q2 q3 q4 g h real;
+syms q1 q2 q3 q4 wx wy wz g h real;
 
-h = [quatrotate([0; 0; g], [q1; q2; q3; q4]); quatrotate([h; 0; 0], [q1; q2; q3; q4])];
-x = [q1; q2; q3; q4];
+h = [quatrotate([0; 0; g], [q1; q2; q3; q4]); quatrotate([h; 0; 0], [q1; q2; q3; q4]); wx; wy; wz];
+x = [q1; q2; q3; q4; wx; wy; wz];
 
 H = jacobian(h, x)
 
